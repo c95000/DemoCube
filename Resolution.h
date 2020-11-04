a3cad1b9aab8032043f3128599642f8dca000cce
jaylon.cheng <jaylon.cheng@peliplat.com>
2020-11-04 08:54:29 +0800

    adapt to double screen

--- Resolution.h
+++ Resolution.h
@@ -14,15 +14,20 @@ private:
     {
         int desktop_width = QApplication::desktop()->width();
         int desktop_height = QApplication::desktop()->height();
-        printf("desktop %d x %d", desktop_width, desktop_height);
+//        printf("desktop %d x %d", desktop_width, desktop_height);
+        QApplication::desktop()->screenNumber();
+//        printf("QApplication::desktop()->screenNumber(): %d", QApplication::desktop()->screenNumber());
+
+        sy = desktop_height / 1080.0;
+        if(sy <= 0) {
+            sy = 1.0;
+        }
+
+        desktop_width = desktop_height * 16 / 9;
 
         sx = desktop_width / 1920.0;
         if(sx <= 0) {
             sx = 1.0;
-        }
-        sy = desktop_height / 1080.0;
-        if(sy <= 0) {
-            sy = 1.0;
         }
 
         printf("Resolution: %lf x %lf", sx, sy);
